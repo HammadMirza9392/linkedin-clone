06ccbe5223029c8d356c672de952fc5374e317e5
Hammad Mirza <hammadhassanmirza@outlook.com>
2021-10-17 00:23:45 +0500

    Header

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function header() {
+    return (
+        <div className="header">
+
+            <div className="header_left">
+                <div className="header_logo">
+                    <img src="https://cdn-icons.flaticon.com/png/512/1377/premium/1377213.png?token=exp=1634412261~hmac=7ab30bba31397d6b4b41ab95aed710bd" />
+                </div>
+                <div className="header_search">
+                    
+                </div>
+            </div>
+
+            <div className="header_right">
+
+            </div>
+        </div>
+    )
+}
+
+export default header
